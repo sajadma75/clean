c1a4e35fd0efbe5087bd60d485bf23da5ea0d684
sajad <64682270+sajadma75@users.noreply.github.com>
2026-06-09 18:33:41 +0330

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-echo "Downloading latest Xray core..."
 
-# دانلود آخرین نسخه (که xhttp رو کامل پشتیبانی می‌کنه)
-wget -q -O core.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+echo "downloading xray"
+wget -O ${PWD}/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
 
-unzip -q core.zip
-chmod +x xray
-mv xray /usr/local/bin/core
+echo "installing"
+unzip xray.zip && chmod +x xray
+mv xray /usr/local/bin/xray
 
-# پاک کردن فایل‌های اضافی
-rm -f core.zip *.dat geosite.dat geoip.dat
-echo "✅ Latest Xray core installed successfully."
+rm -rf ${PWD}/*
+echo "installed!"
